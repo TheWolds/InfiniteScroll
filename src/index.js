6cad3708c55a5e34dc54a9c28a77f11752790cb7
project42da <cmw9101@konkuk.ac.kr>
2019-05-03 19:30:51 +0900

    :wrench: tar 버전수정하고 precommit추가

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-import InfiniteScroll from "./scroll";
+import InfiniteScroll from './scroll';
 import './style.scss';
 
 const itemHTML = (rowHeight, index, data) => {
-    let top = index * rowHeight;
-    return `<li class="item" style="position:absolute; top:${top}px;" data-index=${index}>
-        ${data.name}
-    </li>`
-}
+  let top = index * rowHeight;
+  return `<li class="item" style="position:absolute; top:${top}px;" data-index=${index}>
+      ${data.name}
+    </li>`;
+};
 
 async function fetchData(query) {
-    const response = await fetch(`https://swapi.co/api/people/?page=${query + 1}`);
-    this.options.query = query + 1;
-    const data = await response.json();
-    return data.results;
+  const response = await fetch(`https://swapi.co/api/people/?page=${query + 1}`);
+  this.options.query = query + 1;
+  const data = await response.json();
+  return data.results;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch(`https://swapi.co/api/people/?page=1`).then(res => {
-    // fetch(`https://jsonplaceholder.typicode.com/comments`).then(res => {
-        if (res.status === 200) {
-            return res.json();
-        } else {
-            throw new Error("Error");
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(`https://swapi.co/api/people/?page=1`)
+    .then(res => {
+      // fetch(`https://jsonplaceholder.typicode.com/comments`).then(res => {
+      if (res.status === 200) {
+        return res.json();
+      } else {
+        throw new Error('Error');
+      }
+    })
+    .then(data => {
+      const scroll = InfiniteScroll.setScroll({
+        componentSelector: '#component',
+        parentSelector: '#list',
+        rowSelector: '.item',
+        dataList: data.results,
+        rowHeight: 50,
+        templateHTML: itemHTML,
+        options: {
+          query: 1,
+          isPending: false
         }
-    }).then(data => {
-        const scroll = InfiniteScroll.setScroll({
-            componentSelector: '#component',
-            parentSelector: '#list',
-            rowSelector: '.item',
-            dataList: data.results,
-            rowHeight: 50,
-            templateHTML: itemHTML,
-            options:{
-                query: 1,
-                isPending: false
-            }
-        });
+      });
 
-        scroll.on({
-            fetchData
-        });
+      scroll.on({
+        fetchData: fetchData
+      });
     });
 });
